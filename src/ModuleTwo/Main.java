0ef3ccc67255b6b706918ea5e886dabc03ddb4b7
gourab-sinha <gourab19964u@gmail.com>
2020-11-07 08:34:01 +0530

    Array in Java

---
+++ src/ModuleTwo/Main.java
@@ -0,0 +1,14 @@
+package ModuleTwo;
+
+import ModuleTwo.Array.Array;
+import ModuleTwo.StringManipulation.StringLang;
+
+public class Main {
+    public static void main(String[] args) {
+        Array.singleDArray();
+        Array.twoDArray();
+        Array.singleDArrayInitialized();
+        Array.twoDArrayInitialized();
+
+    }
+}
